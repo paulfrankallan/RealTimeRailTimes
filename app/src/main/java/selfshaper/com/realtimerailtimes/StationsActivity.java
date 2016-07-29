d3c3d61d89bb85ac69bf829df0311b7be889ebb1
Paul Frank Allan <p.f.allan@gmail.com>
2016-07-29 23:33:35 +0100

    initial commit

---
+++ app/src/main/java/selfshaper/com/realtimerailtimes/StationsActivity.java
@@ -0,0 +1,17 @@
+package selfshaper.com.realtimerailtimes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StationsActivity extends AppCompatActivity {
+
+    // retrofit
+    // butterKnife
+    // eventbus
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stations);
+    }
+}
