91b04cb753fcf029f157f472ebcdbdd59f43a518
Paul Frank Allan <p.f.allan@gmail.com>
2016-07-30 00:34:39 +0100

    added stations fragment

--- app/src/main/java/selfshaper/com/realtimerailtimes/StationsActivity.java
+++ app/src/main/java/selfshaper/com/realtimerailtimes/StationsActivity.java
@@ -1,7 +1,11 @@
 package selfshaper.com.realtimerailtimes;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class StationsActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class StationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stations);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_stations, container, false);
+            return rootView;
+        }
     }
 }
